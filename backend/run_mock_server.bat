1f3f2a09f8c13537ee674f244b0f88db2ab7b580
jeevan-vishal <jeevanvishal2006gd@gmail.com>
2026-08-20 20:23:10 +0530

    Delete backend directory

--- backend/run_mock_server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\admin\OneDrive\Desktop\worker-committee-app\backend"
-"C:\Users\admin\OneDrive\Desktop\worker-committee-app\backend\venv\Scripts\python.exe" -m uvicorn mock_server:app --host 127.0.0.1 --port 8000
